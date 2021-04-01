39367c537bd35e940959facc873effdc88503135
Dawid Sawicki <sawickidawid@outlook.com>
2021-04-01 21:50:39 +0200

    Playing with headers endpoint, refactor core/responses.py, refactor api/api.py

--- http_responses_app/api/api.py
+++ http_responses_app/api/api.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
-from http_responses_app.api.endpoints import client_error, server_error, successful
+from http_responses_app.api.endpoints import playing_with_headers, client_error, server_error, successful
 
 api_router = APIRouter()
 
-api_router.include_router(successful.router, tags=["Successful requests responses"])
-api_router.include_router(client_error.router, tags=["Client error requests responses"])
-api_router.include_router(server_error.router, tags=["Server error requests responses"])
+api_router.include_router(router=successful.router,
+                          tags=["Successful requests responses"])
+api_router.include_router(router=client_error.router,
+                          tags=["Client error requests responses"])
+api_router.include_router(router=server_error.router,
+                          tags=["Server error requests responses"])
+api_router.include_router(router=playing_with_headers.router,
+                          tags=["Playing with headers responses"])
--- http_responses_app/core/responses.py
+++ http_responses_app/core/responses.py
@@ -7,28 +7,39 @@ from http_responses_app.core.response_codes_information import get_code_informat
 from http_responses_app.model.pong import Pong
 
 
-def get_response(status_code: int):
-    return __response(status_code, "GET")
+def get_response(status_code: int, additional_info: dict = None):
+    return __response(status_code=status_code,
+                      method="GET",
+                      additional_info=additional_info)
 
 
 def post_response(status_code: int, request_body: Pong):
-    return __response(status_code, "POST", request_body)
+    return __response(status_code,
+                      method="POST",
+                      request_body=request_body)
 
 
 def put_response(status_code: int, request_body: Pong):
-    return __response(status_code, "PUT", request_body)
+    return __response(status_code,
+                      method="PUT",
+                      request_body=request_body)
 
 
-def __response(status_code: int, method: str, request_body: Optional[Pong] = None) -> JSONResponse:
-    content = {}
-    if request_body is not None:
-        content = {
-            "yourRequestBody": request_body.request_body
-        }
+def __response(status_code: int, method: str,
+               request_body: Optional[Pong] = None,
+               additional_info: dict = None) -> JSONResponse:
+    content = {"yourRequestBody": request_body.request_body} if request_body else {}
+
+    if not additional_info:
+        additional_info = {}
+
+    additional_info.update(get_code_information(status_code))
+
     content.update({
         "basicInformation": f"This is {method} example for status code {status_code}",
-        "responseInformation": get_code_information(status_code),
+        "responseInformation": additional_info,
         "timestamp": f"{datetime.now()}"})
+
     return JSONResponse(
         status_code=status_code,
         content=content
