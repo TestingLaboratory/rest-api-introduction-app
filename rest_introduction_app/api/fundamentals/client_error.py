21b15f97d37694e1dee2ff066febbc7e4406e59e
Mjusick <paw.muzyk@gmail.com>
2021-04-07 20:13:46 +0200

    get response 403

--- rest_introduction_app/api/fundamentals/client_error.py
+++ rest_introduction_app/api/fundamentals/client_error.py
@@ -1,3 +1,17 @@
 from fastapi import APIRouter
-
+from http_responses_app.core.responses import get_response
 router = APIRouter()
+
+data = {}
+
+
+@router.on_event("startup")
+def startup_event():
+    data["quota"] = 3
+    data["limit"] = 0
+
+
+@router.get("/limited")
+async def get_response_403():
+    data["limit"] += 1
+    return get_response(200 if data["limit"] < data["quota"] else 403)
