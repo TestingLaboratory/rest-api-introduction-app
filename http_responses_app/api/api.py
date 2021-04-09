5ccd5621d8bfa37c0fc7a6276f9ea266376a3e93
tszyborski <tszyborski@sii.pl>
2021-04-09 17:45:30 +0200

    cookies

--- http_responses_app/api/api.py
+++ http_responses_app/api/api.py
@@ -4,7 +4,7 @@ from http_responses_app.api.challenges.challenge_1 import challenge_1
 from http_responses_app.api.challenges.challenge_2 import challenge_2
 from http_responses_app.api.challenges.challenge_primer import challenge_primer
 from http_responses_app.api.fundamentals import playing_with_headers, client_error, server_error, successful, \
-    basic_authentication, serving_images
+    basic_authentication, serving_images, cookies
 
 api_router = APIRouter()
 
@@ -20,6 +20,8 @@ api_router.include_router(router=basic_authentication.router,
                           tags=["Basic auth example"])
 api_router.include_router(router=serving_images.router,
                           tags=["Serving images"])
+api_router.include_router(router=cookies.router,
+                          tags=["Cookies"])
 api_router.include_router(router=challenge_primer.router,
                           tags=["Challenge Primer - A warm-up for OUR testers!"])
 api_router.include_router(router=challenge_1.router,
