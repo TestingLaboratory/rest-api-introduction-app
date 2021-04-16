916e80634e77c7effe3b7f958fcf380ab43e0f44
Paweł Muzyka <43609680+Mjusick@users.noreply.github.com>
2021-04-16 12:15:42 +0200

    Challenge_3

--- rest_introduction_app/api/fundamentals/client_error.py
+++ rest_introduction_app/api/fundamentals/client_error.py
@@ -1,3 +1,31 @@
-from fastapi import APIRouter
+import secrets
+
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from starlette import status
+
+from .core.responses import get_response
 
 router = APIRouter()
+
+security = HTTPBasic()
+
+
+@router.get("/limited")
+def get_limited_resource(credentials: HTTPBasicCredentials = Depends(security)):
+    """
+    Username: Captain_snack, password: LateNightSausage
+    """
+    correct_username = secrets.compare_digest(credentials.username, "Captain_snack")
+    correct_password = secrets.compare_digest(credentials.password, "LateNightSausage")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"The sausage is not allowed for the {credentials.username}."
+        )
