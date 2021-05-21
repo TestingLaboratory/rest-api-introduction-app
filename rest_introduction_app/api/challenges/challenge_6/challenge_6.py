d96a036b10d1ef25ed83e8247233602f91a31f6d
Mjusick <paw.muzyk@gmail.com>
2021-05-21 18:56:00 +0200

    challange_6 puts and patches

--- rest_introduction_app/api/challenges/challenge_6/challenge_6.py
+++ rest_introduction_app/api/challenges/challenge_6/challenge_6.py
@@ -6,7 +6,9 @@ Diatlov Pass
 (restart endpoint needed!)
 - you have to set up a tent and go to sleep to get your flag
 """
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, Query
 from fastapi.params import Path
 from starlette import status
 from starlette.exceptions import HTTPException
@@ -16,22 +18,24 @@ from rest_introduction_app.api.challenges.challenge_6.model import Hiker, Item
 
 challenge_tag = "Challenge - Excursions on Diatlov Pass"
 router = APIRouter(prefix="/challenge/diatlov-pass")
+
 hiker = Hiker()
 
-
+# TODO add validation for pocket size items
 # TODO fill in responses or delete them
 item_responses = {
-        "tent": "It is nice to have a piece of cloth over your head in the middle of dark woods...",
-        "knife": "Remember to sharpen it MacGyver.",
-        "torch": "",
-        "jacket": "",
+        "map": "Probably a good choice... not to follow the voices in your head.",
+        "tent": "It is nice to have a piece of cloth over your head in the middle of nowhere...",
+        "knife": "Keep it sharp. You never know what you might need it for.",
+        "torch": "Shed some light on the unknown course of events.",
+        "camera": "Take pictures and preserve moments for yourself... or others.",
         "lighter": "",
-        "compass": "",
-        "thermos": "",
-        "headlamp": "",
-        "powerbank": "",
-        "smartphone": "",
-        "bottled_water": ""
+        "matches": "",
+        "compass": "Follow the map to reach the northern Ural.",
+        "thermos": "Ahhh... a sip of hot tea reminds of home, doesn't it?",
+        "headlamp": "Go straight ahead, clear the path with a stream of light.",
+        "bottled_water": "",
+        "winter_jacket": "Hope you still own this jacket at the end of this journey :)",
     }
 
 
@@ -52,7 +56,7 @@ async def restart():
 
 @router.get("/backpack_content", status_code=status.HTTP_200_OK)
 async def backpack_content():
-    items = [item.name for item in hiker.backpack.content]
+    items = ",".join([item.name for item in hiker.backpack.content])
     return JSONResponse({
         "backpack_content": f"{items}"
     })
@@ -60,7 +64,7 @@ async def backpack_content():
 
 @router.get("/pocket_content", status_code=status.HTTP_200_OK)
 async def pocket_content():
-    items = [item.name for item in hiker.pocket.content]
+    items = ",".join([item.name for item in hiker.pocket.content])
     return JSONResponse({
         "pocket_content": f"{items}"
     })
@@ -77,7 +81,7 @@ async def add_to_backpack(item: Item):
         raise HTTPException(status_code=400,
                             detail=f"Your backpack is full already.")
 
-# TODO add validation for pocket size items
+
 @router.post("/add_to_pocket/{item}")
 async def add_to_pocket(item: Item):
     if not hiker.pocket.is_full():
@@ -96,7 +100,7 @@ async def swap_item(item_to_replace: Item,
     hiker.backpack.swap_item(item_to_replace, item_to_pack)
     return JSONResponse({
         "message": f"You've decided to take {item_to_pack.name} instead of {item_to_replace.name}. "
-                   f"Remember, all that matters now is to survive."
+                   f"Remember, all that matters is to survive."
     })
 
 
@@ -106,5 +110,37 @@ async def swap_item(item_to_replace: Item,
     hiker.pocket.swap_item(item_to_replace, item_to_pack)
     return JSONResponse({
         "message": f"You've decided to take {item_to_pack.name} instead of {item_to_replace.name}. "
-                   f"Remember, all that matters now is to survive."
+                   f"Remember, all that matters is to survive."
     })
+
+
+@router.put("/pack_all_to_backpack", status_code=status.HTTP_201_CREATED)
+async def pack_all_to_backpack(items: List[Item] = Query(...)):
+    hiker.backpack.put_items(items)
+    return JSONResponse(
+        {"message": "You've packed in a rush, huh? Do you have that strong feeling that you forgot something?"}
+    )
+
+
+@router.put("/pack_all_to_pocket", status_code=status.HTTP_201_CREATED)
+async def pack_all_to_pocket(items: List[Item] = Query(...)):
+    hiker.pocket.put_items(items)
+    return JSONResponse(
+        {"message": "You've packed in a rush, huh? Do you have that strong feeling that you forgot something?"}
+    )
+
+
+@router.delete("/remove_from_backapack", status_code=status.HTTP_200_OK)
+async def remove_from_backpack(item: Item):
+    hiker.backpack.remove_item(item)
+    return JSONResponse(
+        {"message": f"{item} has been removed from your backpack."}
+    )
+
+
+@router.delete("/remove_from_pocket", status_code=status.HTTP_200_OK)
+async def remove_from_pocket(item: Item):
+    hiker.pocket.remove_item(item)
+    return JSONResponse(
+        {"message": f"{item} has been removed from your pocket."}
+    )
--- rest_introduction_app/api/challenges/challenge_6/model.py
+++ rest_introduction_app/api/challenges/challenge_6/model.py
@@ -1,31 +1,35 @@
 import enum
 from dataclasses import dataclass
+from typing import List
 
+from pydantic import BaseModel
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse
 
 
 class Item(str, enum.Enum):
+    map = "map"
     tent = "tent"
     knife = "knife"
     torch = "torch"
-    jacket = "jacket"
+    camera = "camera"
     matches = "matches"
     lighter = "lighter"
     compass = "compass"
     thermos = "thermos"
     headlamp = "headlamp"
-    powerbank = "powerbank"
-    smartphone = "smartphone"
     bottled_water = "bottled water"
+    winter_jacket = "winter_jacket"
 
 
 @dataclass
 class Storage:
+    storage_type: str
     max_item_number: int
     content: list
 
-    def __init__(self, max_item_number: int):
+    def __init__(self, storage_type: str, max_item_number: int):
+        self.storage_type = storage_type
         self.max_item_number = max_item_number
         self.content = []
 
@@ -34,6 +38,20 @@ class Storage:
 
     def add_item(self, item: Item):
         self.content.append(item)
+
+    def remove_item(self, item: Item):
+        if item in self.content:
+            self.content.remove(item)
+        else:
+            raise HTTPException(status_code=400,
+                                detail=f"Hmm, are you sure you've packed {item}?")
+
+    def put_items(self, items: List[Item]):
+        if len(items) <= self.max_item_number:
+            self.content = items
+        else:
+            raise HTTPException(status_code=400,
+                                detail=f"You can only pack {self.max_item_number} items in your {self.storage_type}")
 
     def swap_item(self, item_to_replace, item_to_pack):
         if item_to_replace in self.content:
@@ -50,9 +68,10 @@ class Hiker:
     pocket: Storage
 
     def __init__(self):
-        self.backpack = Storage(5)
-        self.pocket = Storage(2)
+        self.backpack = Storage("backpack", 5)
+        self.pocket = Storage("pocket", 2)
 
     def set_to_default(self):
-        self.backpack = Storage(5)
-        self.pocket = Storage(2)
+        self.backpack = Storage("backpack", 5)
+        self.pocket = Storage("pocket", 2)
+
