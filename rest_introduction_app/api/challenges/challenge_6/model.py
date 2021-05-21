0d57220df1e2b6d403505da85569cae600e2463b
Mjusick <paw.muzyk@gmail.com>
2021-05-21 19:22:06 +0200

    repo repair

--- rest_introduction_app/api/challenges/challenge_6/model.py
+++ rest_introduction_app/api/challenges/challenge_6/model.py
@@ -1,59 +1,35 @@
 import enum
 from dataclasses import dataclass
-<<<<<<< HEAD
 from typing import List
 
 from pydantic import BaseModel
-=======
-
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse
 
 
 class Item(str, enum.Enum):
-<<<<<<< HEAD
     map = "map"
     tent = "tent"
     knife = "knife"
     torch = "torch"
     camera = "camera"
-=======
-    tent = "tent"
-    knife = "knife"
-    torch = "torch"
-    jacket = "jacket"
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
     matches = "matches"
     lighter = "lighter"
     compass = "compass"
     thermos = "thermos"
     headlamp = "headlamp"
-<<<<<<< HEAD
     bottled_water = "bottled water"
     winter_jacket = "winter_jacket"
-=======
-    powerbank = "powerbank"
-    smartphone = "smartphone"
-    bottled_water = "bottled water"
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
 
 
 @dataclass
 class Storage:
-<<<<<<< HEAD
     storage_type: str
     max_item_number: int
     content: list
 
     def __init__(self, storage_type: str, max_item_number: int):
         self.storage_type = storage_type
-=======
-    max_item_number: int
-    content: list
-
-    def __init__(self, max_item_number: int):
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
         self.max_item_number = max_item_number
         self.content = []
 
@@ -63,7 +39,6 @@ class Storage:
     def add_item(self, item: Item):
         self.content.append(item)
 
-<<<<<<< HEAD
     def remove_item(self, item: Item):
         if item in self.content:
             self.content.remove(item)
@@ -78,8 +53,6 @@ class Storage:
             raise HTTPException(status_code=400,
                                 detail=f"You can only pack {self.max_item_number} items in your {self.storage_type}")
 
-=======
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
     def swap_item(self, item_to_replace, item_to_pack):
         if item_to_replace in self.content:
             itr_index = self.content.index(item_to_replace)
@@ -95,7 +68,6 @@ class Hiker:
     pocket: Storage
 
     def __init__(self):
-<<<<<<< HEAD
         self.backpack = Storage("backpack", 5)
         self.pocket = Storage("pocket", 2)
 
@@ -103,11 +75,3 @@ class Hiker:
         self.backpack = Storage("backpack", 5)
         self.pocket = Storage("pocket", 2)
 
-=======
-        self.backpack = Storage(5)
-        self.pocket = Storage(2)
-
-    def set_to_default(self):
-        self.backpack = Storage(5)
-        self.pocket = Storage(2)
->>>>>>> d89d654d25007b9ecd9a4bd324bc3e19c33eac12
