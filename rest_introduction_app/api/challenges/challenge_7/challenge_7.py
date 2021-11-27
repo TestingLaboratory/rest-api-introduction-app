790ed4f5667cc07854b5e908e5381860d06ef6a2
Mjusick <paw.muzyk@gmail.com>
2021-11-27 18:30:15 +0100

    challenge_7 init idea

---
+++ rest_introduction_app/api/challenges/challenge_7/challenge_7.py
@@ -0,0 +1,8 @@
+"""
+SARS-Cov-2
+- you are a SARS-Cov-2 virus with initial RNA sequence
+- you need to infect individuals to mutate
+- mutations gives you ability to infect different species
+- infecting new species gives you new abilities to mutate/ more effective mutations (?)
+TBD
+"""
